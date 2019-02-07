ce0931a09f9e9bc6752ab63762c1565cf9e965ae
Jennifer Busca <jennifer.busca@gmail.com>
2019-02-07 13:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) 
+if string === string.toLowerCase
+return "I can't hear you!" 
+else
+     ReferenceError: sayHiToGrandma is not defined
+      at Context.<anonymous> (test/index-test.js:40:5)
+
+  5) sayHiToGrandma(string) returns "YES INDEED!" if `string` is uppercase:
+     ReferenceError: sayHiToGrandma is not defined
+      at Context.<anonymous> (test/index-test.js:44:5)
+
+  6) sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."`:
+     ReferenceError: sayHiToGrandma is not defined
+      at Context.<anonymous> (test/index-test.js:48:5)
